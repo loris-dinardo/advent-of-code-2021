d1ff3900191864f4ac718fe9341d127f66e7e2cb
loris-dinardo <loris.dinardo@gmail.com>
2021-12-03 11:20:21 +0100

    Setup project

---
+++ src/jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const baseJestConfig = require("../jest.config");
+
+module.exports = {
+    ...baseJestConfig,
+    testEnvironment: "node",
+    testMatch: [
+        "**/__tests__/**/*.test.ts?(x)",
+    ],
+};
